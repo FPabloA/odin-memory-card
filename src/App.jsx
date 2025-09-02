8dcd78cc84b6d576ec9da85b940dee0a800a433d
FPabloA <pabloayala@protonmail.com>
2025-09-02 10:19:57 -0500

    finished fetching of pokemon data and population of cards with said data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [cards, setCards] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const [cards, setCards] = useState([]);
   const [score, changeScore] = useState(0);
   const [bestScore, changeBestScore] = useState(0);
   const [clickedCards, setClickedCards] = useState([]);
 
+  // const genRandom = (min=1, max=139) => {
+  //   return Math.floor(Math.random() * (max - min + 1) + min)
+  // }
+
+  useEffect(() => {
+    getCards();
+  }, []);
+
+  async function getCards() {
+    const startID = Math.floor(Math.random() * (139 - 1 + 1) + 1);
+    const arr = [];
+    for (let i = startID; i < startID+12; i++){
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const data = await response.json();
+      arr.push(data);
+    }
+    setCards(arr);
+    console.log(arr[0]);
+  }
+
   //uses fisher-yates shuffle
-  function shuffle(array) {
+  const shuffle = (array) => {
   let currentIndex = array.length;
 
   // While there remain elements to shuffle...
@@ -47,9 +67,10 @@ function App() {
     }
 
     shuffle([...cards]);
-    console.log(score);
   }
 
+  
+  
   return (
     <>
       <div className='score-tracker'>
@@ -58,11 +79,14 @@ function App() {
         </div>
       {
         cards.map((item) => {
-          return <Card text={item} handleClick={() => cardClick(item)}></Card>;
+          return <Card text={item.name} imgURL={item.sprites.front_default} handleClick={() => cardClick(item.name)}></Card>;
         })
       }
     </>
   )
+  
+
+  
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import '../styles/Card.css'
 
-function Card ({ text = "", handleClick }) {
+function Card ({ text = "", imgURL, handleClick }) {
     return (
         <>
             <div className="card-wrapper" onClick={handleClick}>
-                <div className="card-img">IMG placeholder</div>
+                <div className="card-img-wrapper">
+                    <img className='card-img' src={imgURL}></img>
+                </div>
                 <div className="card-text-wrapper">
                     <div className="card-text">{text}</div>
                 </div>
